e8482fb0ec2d71620f5fefc087bb5d975db41251
Chanthorn Yoeun <chanthorn.developer@gmail.com>
2022-04-09 22:39:03 +0700

    Fix missing API endpoint & Add EmployeeService

--- src/app/enums/api-endpiont.enum.ts
+++ src/app/enums/api-endpiont.enum.ts
@@ -1,5 +1,7 @@
 export enum ApiEndPointEnum {
     POSITION = '/api/position',
+    EMPLOYEE = '/api/employee',
+    DEPARTMENT = '/api/department',
     LOGIN = '/api/auth/login',
     LOGOUT = '/api/auth/logout'
 }
---
+++ src/app/services/employee.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { RestfulService } from '../core/http/restful.service';
+import { ApiEndPointEnum } from '../enums/api-endpiont.enum';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService extends RestfulService {
+
+  constructor(http: HttpClient) { 
+    super(http);
+  }
+
+  getUrl(): string {
+    return ApiEndPointEnum.EMPLOYEE;
+  }
+
+}
