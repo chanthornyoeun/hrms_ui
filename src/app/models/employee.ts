26d509ee620c55548a25bba3db8f653426a53e54
Chanthorn Yoeun <chanthorn.developer@gmail.com>
2022-04-09 22:40:46 +0700

    Add Employee model

---
+++ src/app/models/employee.ts
@@ -0,0 +1,29 @@
+import { Department } from "./department";
+import { Position } from "./position";
+
+export interface Employee {
+    id: number;
+    firstName: string;
+    lastName: string;
+    name: string;
+    title: string;
+    dateOfBirth: Date;
+    gender: string;
+    joinedDate: string;
+    resignDate?: Date;
+    email: string;
+    phone: string;
+    currentAddress: string;
+    physicalAddress: string;
+    maritalStatus: string;
+    isActive: boolean;
+    jobTitle: string;
+    createdById?: number;
+    updatedById?: number;
+    departmentId: number;
+    positionId: number;
+    createdAt: Date;
+    updatedAt: Date;
+    position: Position;
+    department: Department;
+}
