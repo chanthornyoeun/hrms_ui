ce199371394dbf10141881ca3d0c168b02174350
Chanthorn Yoeun <chanthorn.developer@gmail.com>
2022-05-26 01:27:54 +0700

    Add remember pagination to dependent-type

--- src/app/modules/dependent-type/dependent-type-list/dependent-type-list.component.ts
+++ src/app/modules/dependent-type/dependent-type-list/dependent-type-list.component.ts
@@ -1,21 +1,19 @@
-import { HttpParams } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { Component, ViewChild } from '@angular/core';
 import { DependentType } from 'src/app/models/dependent-type';
 import { DependentTypeService } from 'src/app/services/dependent-type.service';
-import { ColumnConfig } from 'src/app/shared/components/data-grid/data-grid.component';
+import { ColumnConfig, DataGridComponent } from 'src/app/shared/components/data-grid/data-grid.component';
 import { Pagination } from 'src/app/shared/components/data-grid/pagination';
 import { LoaderService } from "../../../shared/components/loader/loader.service";
-import { finalize } from "rxjs/operators";
+import { PaginationHistoryService } from 'src/app/services/pagination-history.service';
 
 @Component({
   selector: 'app-dependent-type-list',
   templateUrl: './dependent-type-list.component.html',
   styleUrls: ['./dependent-type-list.component.scss']
 })
-export class DependentTypeListComponent extends Pagination {
+export class DependentTypeListComponent extends Pagination<DependentType> {
 
-  dependentType$!: Observable<DependentType[]>;
+  @ViewChild(DataGridComponent) grid!: DataGridComponent;
 
   config: ColumnConfig = {
     columnDefs: [
@@ -33,21 +31,10 @@ export class DependentTypeListComponent extends Pagination {
 
   constructor(
     private dependentTypeService: DependentTypeService,
-    private loaderService: LoaderService
+    protected override loaderService: LoaderService,
+    private pg: PaginationHistoryService
   ) {
-    super();
-    this.list(this.params);
-  }
-
-  list(params: HttpParams) {
-    this.loaderService.show();
-    this.dependentType$ = this.dependentTypeService.list({ params }).pipe(
-      finalize(() => this.loaderService.hide()),
-      map(res => {
-        this.total = res.total;
-        return res.data as DependentType[];
-      })
-    );
+    super(dependentTypeService, loaderService, pg);
   }
 
 }
