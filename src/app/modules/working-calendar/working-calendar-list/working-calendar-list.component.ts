8674773d64e90217899da726b0ac16f2d970d11f
Chanthorn Yoeun <chanthorn.developer@gmail.com>
2022-08-17 23:06:18 +0700

    Add filter working calendar by department

--- src/app/modules/working-calendar/working-calendar-list/working-calendar-list.component.ts
+++ src/app/modules/working-calendar/working-calendar-list/working-calendar-list.component.ts
@@ -1,19 +1,27 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { WorkingCalendar } from 'src/app/models/working-calendar';
 import { WorkingCalendarService } from 'src/app/services/working-calendar.service';
 import { ColumnConfig, DataGridComponent } from 'src/app/shared/components/data-grid/data-grid.component';
 import { Pagination } from 'src/app/shared/components/data-grid/pagination';
 import { LoaderService } from "../../../shared/components/loader/loader.service";
 import { PaginationHistoryService } from 'src/app/services/pagination-history.service';
+import { FormControl } from '@angular/forms';
+import { Observable, map } from 'rxjs';
+import { Department } from '../../../models/department';
+import { DepartmentService } from '../../../services/department.service';
+import { ParamsBuilder } from '../../../utilities/params-builder';
+import { HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-working-calendar-list',
   templateUrl: './working-calendar-list.component.html',
   styleUrls: ['./working-calendar-list.component.scss']
 })
-export class WorkingCalendarListComponent extends Pagination<WorkingCalendar> {
+export class WorkingCalendarListComponent extends Pagination<WorkingCalendar> implements OnInit {
 
   @ViewChild(DataGridComponent) grid!: DataGridComponent;
+  departmentCtl: FormControl = new FormControl('All');
+  department$!: Observable<Department[]>;
 
   config: ColumnConfig = {
     columnDefs: [
@@ -33,10 +41,37 @@ export class WorkingCalendarListComponent extends Pagination<WorkingCalendar> {
 
   constructor(
     private workingCalendarService: WorkingCalendarService,
+    private departmentService: DepartmentService,
     protected override loaderService: LoaderService,
     private pg: PaginationHistoryService
   ) {
     super(workingCalendarService, loaderService, pg);
+    const queryParams: { [key: string]: any } = this.pg.getQueryParams();
+    if (queryParams['departmentId']) {
+      this.departmentCtl.setValue(queryParams['departmentId']);
+    }
+  }
+
+  ngOnInit(): void {
+    this.department$ = this.departmentService.list({ params: ParamsBuilder.build({ limit: 100 }) }).pipe(map(res => res.data));
+    this.departmentChanged();
+  }
+
+  private departmentChanged() {
+    this.departmentCtl.valueChanges.subscribe(departmentId => {
+      this.paginationHistoryService.updateQueryParams({ departmentId });
+      let params: HttpParams = new HttpParams();
+      if (departmentId !== 'All') {
+        params = ParamsBuilder.build({ departmentId });
+        this.paginationHistoryService.setPreviousPagination({ ...this.pagination });
+        this.pagination.pageIndex = 0;
+        this.pagination.offset = 0;
+      } else {
+        this.paginationHistoryService.updateQueryParams({});
+        this.pagination = this.paginationHistoryService.getPreviousPagination();
+      }
+      this.list(this.pagination, params);
+    });
   }
 
 }
--- src/app/modules/working-calendar/working-calendar.module.ts
+++ src/app/modules/working-calendar/working-calendar.module.ts
@@ -5,19 +5,11 @@ import { WorkingCalendarRoutingModule } from './working-calendar-routing.module'
 import { WorkingCalendarFormComponent } from './working-calendar-form/working-calendar-form.component';
 import { WorkingCalendarListComponent } from './working-calendar-list/working-calendar-list.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { WorkingPipe } from './working.pipe';
-import { MatSelectModule } from '@angular/material/select';
-import { NgxMatTimepickerModule } from 'ngx-mat-timepicker'; 
+import { NgxMatTimepickerModule } from 'ngx-mat-timepicker';
 import { PaginationHistoryService } from 'src/app/services/pagination-history.service';
+import { MaterialCompModule } from '../../shared/material-comp/material-comp.module';
 
 
 @NgModule({
@@ -29,18 +21,10 @@ import { PaginationHistoryService } from 'src/app/services/pagination-history.se
   imports: [
     CommonModule,
     WorkingCalendarRoutingModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatButtonModule,
-    MatIconModule,
-    MatInputModule,
-    MatTooltipModule,
-    MatSlideToggleModule,
-    MatSelectModule,
     ReactiveFormsModule,
     NgxMatTimepickerModule,
-    SharedModule
+    SharedModule,
+    MaterialCompModule,
   ],
   providers: [
     PaginationHistoryService
