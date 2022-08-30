7bd02c28705bce2412279dd4fc1297c1d4630743
Chanthorn Yoeun <chanthorn.developer@gmail.com>
2022-08-30 13:38:52 +0700

    Update API URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL: 'http://174.138.24.155:8090',
+  API_URL: 'https://hr-ocean.com:8090',
   X_API_KEY: 'aHJtcy1wcm9qZWN0LXhhcGlrZXktc2Vj',
   dateFormat: 'dd-MMM-yyyy',
   dateTimeFormat: 'dd-MMM-yyyy hh:mm a',
