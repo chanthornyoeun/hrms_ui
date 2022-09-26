0810646702d838f499e3a04956a89eaf1b4b95b1
Chanthorn Yoeun <chanthorn.developer@gmail.com>
2022-09-26 14:00:30 +0700

    Add employee search form to employee's card-view

--- src/app/modules/employee/employee-views/card-view/card-view.component.ts
+++ src/app/modules/employee/employee-views/card-view/card-view.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { finalize, map } from 'rxjs';
 import { Employee } from 'src/app/models/employee';
 import { EmployeeService } from 'src/app/services/employee.service';
+import { PaginationHistoryService } from 'src/app/services/pagination-history.service';
 import { ParamsBuilder } from 'src/app/utilities/params-builder';
+import { EmployeeSearch } from "../../employee-search-form/employee-search";
 
 @Component({
   selector: 'app-card-view',
@@ -19,15 +21,28 @@ export class CardViewComponent implements OnInit {
   isLoading: boolean = false;
 
   constructor(
-    private employeeService: EmployeeService
+    private employeeService: EmployeeService,
+    private paginationHistoryService: PaginationHistoryService
   ) { }
 
   ngOnInit(): void {
-    const params: HttpParams = ParamsBuilder.build({ offset: 0, limit: this.limit });
-    this.getEmployees(params);
+    this.getEmployees(this.getQueryHistory());
   }
 
-  getEmployees(params?: HttpParams): void {
+  private getQueryHistory(): HttpParams {
+    const queryParams: { [key: string]: any } = this.paginationHistoryService.getQueryParams();
+    Object.keys(queryParams).forEach((key: string) => {
+      if (queryParams.hasOwnProperty(key) && queryParams[key] === 'All') {
+        queryParams[key] = null;
+      }
+    });
+    return ParamsBuilder.build({ offset: this.offset, limit: this.limit, ...queryParams });
+  }
+
+  private getEmployees(params?: HttpParams): void {
+    if (!params) {
+      params = ParamsBuilder.build({ offset: 0, limit: this.limit });
+    }
     this.isLoading = true;
     this.employeeService.list({ params })
       .pipe(
@@ -44,8 +59,25 @@ export class CardViewComponent implements OnInit {
   }
 
   onScrollDown(): void {
-    const params: HttpParams = ParamsBuilder.build({ offset: this.offset, limit: this.limit });
+    if (this.total > this.offset) {
+      const queryParams: object = this.paginationHistoryService.getQueryParams();
+      let params: HttpParams = ParamsBuilder.build({ offset: this.offset, limit: this.limit, ...queryParams });
+      this.getEmployees(params);
+    }
+  }
+
+  search($event: EmployeeSearch) {
+    this.employees = [];
+    const params: HttpParams = ParamsBuilder.build({ offset: 0, limit: this.limit, ...$event });
+    this.paginationHistoryService.updateQueryParams($event);
     this.getEmployees(params);
+  }
+
+  clear() {
+    this.offset = 0;
+    this.employees = [];
+    this.paginationHistoryService.updateQueryParams({});
+    this.getEmployees();
   }
 
 }
